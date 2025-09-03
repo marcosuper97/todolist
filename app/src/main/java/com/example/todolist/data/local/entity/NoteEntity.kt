8d452ae9be441253ac0f5254696000033a1b9615
marcosuper97 <gorpishin97@yandex.ru>
2025-09-03 20:14:42 +0300

    Сущность заметок для БД таблицы

---
+++ app/src/main/java/com/example/todolist/data/local/entity/NoteEntity.kt
@@ -0,0 +1,14 @@
+package com.example.todolist.data.local.entity
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "notes")
+data class NoteEntity(
+    @PrimaryKey(autoGenerate = true)
+    val id: Long = 0,
+    val title: String,
+    val description: String? = null,
+    val completed: Boolean = false,
+    val imagePath: String? = null,
+)
