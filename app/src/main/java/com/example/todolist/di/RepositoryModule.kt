9e587c17667879a1e51ee355288a7a2d29a13072
marcosuper97 <gorpishin97@yandex.ru>
2025-09-03 22:25:13 +0300

    di репозиториев

--- app/src/main/java/com/example/todolist/di/RepositoryModule.kt
+++ app/src/main/java/com/example/todolist/di/RepositoryModule.kt
@@ -1,6 +1,17 @@
 package com.example.todolist.di
 
+import com.example.todolist.data.lmpl.ImageRepositoryImpl
+import com.example.todolist.data.lmpl.TasksRepositoryImpl
+import com.example.todolist.domain.repository.ImageRepository
+import com.example.todolist.domain.repository.TasksRepository
 import org.koin.dsl.module
 
 val repositoryModule = module {
+    single<ImageRepository> {
+        ImageRepositoryImpl(get(), get())
+    }
+
+    single <TasksRepository>{
+        TasksRepositoryImpl(get())
+    }
 }
